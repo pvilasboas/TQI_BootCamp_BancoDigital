9dad991e6dc106bfe514fcc38d872c8a322e7bc7
Pedro Roberto <pvilasboas@gmail.com>
2022-06-24 11:21:36 -0300

    Added class ContaCorrente

--- src/com/bootcamp/tqi/bancodigital/ContaCorrente.java
+++ src/com/bootcamp/tqi/bancodigital/ContaCorrente.java
@@ -1,4 +1,14 @@
 package com.bootcamp.tqi.bancodigital;
 
-public class ContaCorrente {
+public class ContaCorrente extends Conta {
+
+    public ContaCorrente(Cliente cliente){
+        super(cliente);
+    }
+
+    public void imprimirExtrato(){
+        System.out.println("Extrato Conta Corrente:");
+        super.imprimirInfosComuns();
+    }
+
 }
