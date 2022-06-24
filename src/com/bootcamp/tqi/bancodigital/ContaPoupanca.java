556eedce5d29b1758c8cd274f9ab3fd618016fb0
Pedro Roberto <pvilasboas@gmail.com>
2022-06-24 11:23:04 -0300

    Added class ContaPoupanca

--- src/com/bootcamp/tqi/bancodigital/ContaPoupanca.java
+++ src/com/bootcamp/tqi/bancodigital/ContaPoupanca.java
@@ -1,4 +1,14 @@
 package com.bootcamp.tqi.bancodigital;
 
-public class ContaPoupanca {
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(Cliente cliente){
+        super(cliente);
+    }
+
+    public void imprimirExtrato(){
+        System.out.println("Extrato Conta Poupanca:");
+        super.imprimirInfosComuns();
+    }
+
 }
